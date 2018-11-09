eb30dd6f26e355044a076f8caaf787cc0e858f22
sebastianslupinski <bezerkey@gmail.com>
2018-11-09 17:12:16 +0100

    first commit

---
+++ src/main/java/com/example/vaadin_crypto_diary/VaadinCryptoDiaryApplication.java
@@ -0,0 +1,12 @@
+package com.example.vaadin_crypto_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinCryptoDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinCryptoDiaryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/vaadin_crypto_diary/VaadinCryptoDiaryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.vaadin_crypto_diary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VaadinCryptoDiaryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
